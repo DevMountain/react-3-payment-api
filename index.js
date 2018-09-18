9dc16001f685130539913665bcc2e5a68ca99edd
James Lemire <webdev.jameslemire@gmail.com>
2018-09-18 14:33:47 -0600

    JL master - Add cors.

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const ctrl = require('./controllers/controller')
 const path = require('path')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(cors())
 
 app.use(express.static(path.join(__dirname, './swagger-ui/dist')))
 app.use('/payment', express.static(path.join(__dirname, './swagger-ui/dist')))
